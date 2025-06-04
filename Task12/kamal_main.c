d8088732767c7363fe9aa15aad1d311d1049a9a6
Dennis Chen <dennischen5022@gmail.com>
2025-06-04 17:18:48 -0400

    complete task 12

--- Task12/kamal_main.c
+++ Task12/kamal_main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Dennis Chen.  All rights reserved.
+ * Copyright (c) 2016 Kamal Heib.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -30,85 +30,103 @@
  * SOFTWARE.
  */
 
-#include "linux/container_of.h"
 #include <linux/slab.h>
 #include <linux/list.h>
 #include <linux/module.h>
 
-MODULE_AUTHOR("Dennis Chen <dechen@redhat.com>");
-MODULE_DESCRIPTION("Little Penguin Task 12");
-MODULE_VERSION("0.1");
-MODULE_LICENSE("GPL");
-
-#define NAME_LEN 20
+MODULE_AUTHOR("Kamal Heib <kamalheib1@gmail.com>");
+MODULE_DESCRIPTION("little penguin Task12");
+MODULE_VERSION("12");
+MODULE_LICENSE("GPL v2");
 
 struct identity {
-	char name[NAME_LEN];
-	int  id;
+	char name[20];
+	int id;
 	bool busy;
 	struct list_head list;
 };
 
-static LIST_HEAD(id_list);
+static LIST_HEAD(identities);
 
 static int identity_create(char *name, int id)
 {
 	struct identity *new;
 
-	new = kzalloc(sizeof(struct identity), GFP_KERNEL);
-	if (!new) {
+	new = kzalloc(sizeof(*new), GFP_KERNEL);
+	if (!new)
 		return -ENOMEM;
-	}
-	
-	snprintf(new->name, NAME_LEN, "%s", name);
+
+	strcpy(new->name, name);
 	new->id = id;
-	new->busy = false;
-	INIT_LIST_HEAD(&new->list);
 
-
-	list_add(&new->list, &id_list);
+	list_add_tail(&new->list, &identities);
 	return 0;
 }
 
-static struct identity *identity_find(int id)
+struct identity *identity_find(int id)
 {
-	struct identity *iter;
-	list_for_each_entry(iter, &id_list, list) {
-		if (iter->id == id)
-			return iter;
+	struct identity *i;
+
+	list_for_each_entry(i, &identities, list) {
+		if (i->id == id)
+			return i;
 	}
+
 	return NULL;
 }
 
-static void identity_destroy(int id)
+void identity_destroy(int id)
 {
-	struct identity *iter, *next;
-	list_for_each_entry_safe(iter, next, &id_list, list) {
-		if (iter->id == id) {
-			list_del(&iter->list);
-			kfree(iter);
-		}
+	struct identity *i, *tmp;
 
+	list_for_each_entry_safe_reverse(i, tmp, &identities, list) {
+		if (i->id == id) {
+			list_del(&i->list);
+			kfree(i);
+			return;
+		}
+	}
+
+	pr_err("Can not destroy id=%d, id not found!\n", id);
+}
+
+void identity_destroy_all(void)
+{
+	struct identity *i, *tmp;
+
+	list_for_each_entry_safe(i, tmp, &identities, list) {
+		list_del(&i->list);
+		kfree(i);
 	}
 }
 
-static int __init t1_init(void)
+static int __init t12_init(void)
 {
-	printk(KERN_DEBUG "Task 12: Hello World!\n");
-
 	struct identity *temp;
+	int err;
 
-	identity_create("Alice", 1);
-	identity_create("Bob", 2);
-	identity_create("Dave", 3);
-	identity_create("Gena", 10);
+	err = identity_create("Alice", 1);
+	if (err)
+		goto destroy;
+
+	err = identity_create("Bob", 2);
+	if (err)
+		goto destroy;
+
+	err = identity_create("Dave", 3);
+	if (err)
+		goto destroy;
+
+	err = identity_create("Gena", 10);
+	if (err)
+		goto destroy;
 
 	temp = identity_find(3);
-	printk(KERN_DEBUG "Task 12: id 3 = %s\n", temp->name);
+	pr_debug("id 3 = %s\n", temp->name);
 
 	temp = identity_find(42);
 	if (temp == NULL)
-		pr_debug("Task 12: id 42 not found\n");
+		pr_debug("id 42 not found\n");
 
 	identity_destroy(2);
 	identity_destroy(1);
@@ -117,17 +135,16 @@ static int __init t1_init(void)
 	identity_destroy(3);
 
 	return 0;
+
+destroy:
+	identity_destroy_all();
+	return err;
 }
 
-static void __exit t1_exit(void)
+static void __exit t12_exit(void)
 {
-	struct identity *iter, *next;
-	list_for_each_entry_safe(iter, next, &id_list, list) {
-		list_del(&iter->list);
-		kfree(iter);
-	}
-	printk(KERN_DEBUG "Task 12: Good bye!\n");
+	identity_destroy_all();
 }
 
-module_init(t1_init);
-module_exit(t1_exit);
+module_init(t12_init);
+module_exit(t12_exit);
